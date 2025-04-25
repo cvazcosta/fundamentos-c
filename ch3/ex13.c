f5c0545187cb963a1648b5fd7dc07bc6a460ad15
cvazcosta <cvazcosta2001@gmail.com>
2025-04-25 16:56:31 -0300

    Calculando tabuada completa por número

---
+++ ch3/ex13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () {
+  int operando;
+  int operador = 0;
+
+  printf("Informe um número: \n");
+  scanf("%d", &operando);
+
+  printf("*** Tabuada de %d *** \n", operando);
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  operador++;
+  printf("%d x %d = %d\n", operando, operador, operando * operador);
+  
+  return 0;
+  }
