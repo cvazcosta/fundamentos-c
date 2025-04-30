41503468a2a20a7f4efc5873f3c0456cbfc5b2d4
cvazcosta <cvazcosta2001@gmail.com>
2025-04-30 13:46:42 -0300

    Praticando o uso de escape characters

---
+++ perry-miller/ch4/escapes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+  printf("Coluna A\tColuna B\tColuna C");
+  printf("\nO som de bip do meu PC é mais ou menos assim: \a!\n");
+  printf("\"Letz\bs fix that typo and then show the backslash ");
+  printf("character \\\" she said \n");
+
+  return 0;
+}
