16215bf11e4f144497ef6b1d41928168c51ffa79
cvazcosta <cvazcosta2001@gmail.com>
2025-05-07 19:43:08 -0300

    Resolvendo a 6ª questão praticando menus e switch

--- ascencio-campos/ch4/ex6.c
+++ ascencio-campos/ch4/ex6.c
@@ -2,34 +2,52 @@
 #include <math.h>
 
 int main () {
-  double n1, n2, resultado;
+  double n1, n2, resultado1, resultado2;
   int opcao;
 
   printf("1 - Elevar o primeiro número ao segundo\n");
   printf("2 - Raiz quadrada de ambos\n");
-  printf("3- Raiz cúbica de ambos\n");
-  printf("Informe a opção desejada: \n");
-  scanf(" %d", opcao);
+  printf("3 - Raiz cúbica de ambos\n");
+  printf("Informe a opção desejada: ");
+  scanf(" %d", &opcao);
 
   switch (opcao)
   {
   case 1:
-    resultado = pow(n1, n2);
-    printf("O número %.2lf elevado a %.2lf é: %.2lf\n", n1, n2, resultado);
+      printf("\nInforme o primeiro número: ");
+      scanf(" %lf", &n1);
+      printf("Informe o segundo número: ");
+      scanf(" %lf", &n2);
+      resultado1 = pow(n1, n2);
+      printf("O número %.2lf^%.2lf é: %.2lf\n", n1, n2, resultado1);
+    break;
+
+  case 2:
+      printf("\nInforme o primeiro número: ");
+      scanf(" %lf", &n1);
+      printf("Informe o segundo número: ");
+      scanf(" %lf", &n2);
+      resultado1 = sqrt(n1);
+      resultado2 = sqrt(n2);
+      printf("\nA raiz quadrada de %.2lf é %.2lf\n", n1, resultado1);
+      printf("A raiz quadrada de %.2lf é %.2lf\n", n2, resultado2);
+    break;
+    
+  case 3:
+      printf("\nInforme o primeiro número: ");
+      scanf(" %lf", &n1);
+      printf("Informe o segundo número: ");
+      scanf(" %lf", &n2);
+      resultado1 = cbrt(n1);
+      resultado2 = cbrt(n2);
+      printf("\nA raiz cúbica de %.2lf é %.2lf\n", n1, resultado1);
+      printf("A raiz cúbica de %.2lf é %.2lf\n", n2, resultado2);
     break;
   
   default:
+      printf("Opção inválida. O programa será encerrado.");
     break;
   }
-
-  // resultado = sqrt(n1);
-  // printf("%.2lf\n", resultado);
-
-  // resultado = cbrt(n2);
-  // printf("%.2lf\n", resultado);
-
-  resultado = pow(2, 3);
-  printf("%.2lf\n", resultado);
-
+ 
   return 0;
 }
