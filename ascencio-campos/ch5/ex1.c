8f50aae5c0e43086d411375ea4be7408f0f8d73b
cvazcosta <cvazcosta2001@gmail.com>
2025-05-16 13:48:22 -0300

    Resolvendo parcialmente o exercício para o caso de grupos de números diferentes entre si

--- ascencio-campos/ch5/ex1.c
+++ ascencio-campos/ch5/ex1.c
@@ -2,58 +2,105 @@
 
 int main () {
   int a, b, c, d;
-  int pri, seg, ter, qua;
-  int nums[16];
+  int nums[100];
+  int ord[100];
+  int rest[100];
   
-  // for (int i = 0; i < 5; i++)
-  // {
-  //   printf("Informe o 1º valor: \n");
-  //   scanf(" %d", &a);
-
-  //   printf("Informe o 2º valor: \n");
-  //   scanf(" %d", &b);
-
-  //   printf("Informe o 3º valor: \n");
-  //   scanf(" %d", &c);
-
-  //   printf("Informe o 4º valor: ");
-  //   scanf(" %d", &d);
-
-  //   printf("\nNúmeros informados: %d, %d, %d, %d\n", a, b, c, d);
-  // }
-
-  printf("Informe o 1º valor: ");
-  scanf(" %d", &a);
-
-  printf("Informe o 2º valor: ");
-  scanf(" %d", &b);
-
-  printf("Informe o 3º valor: ");
-  scanf(" %d", &c);
-
-  printf("Informe o 4º valor: ");
-  scanf(" %d", &d);
-
-  nums[0] = a;
-  nums[1] = b;
-  nums[2] = c;
-  nums[3] = d;
-
-  // printf("%d", nums[0]);
-  // printf("%d", nums[1]);
-  // printf("%d", nums[2]);
-  // printf("%d", nums[3]);  
-
-  for (int i = 0; i < 3; i++)
+  for (int y = 0; y < 5; y++)
   {
-    if (nums[i+1] <= nums[i]) 
-    {
-      nums[0] = nums[i+1];
-    }
-  }
+    printf("Informe o 1º valor: ");
+    scanf(" %d", &nums[0]);
 
-  printf("O menor número é: %d\n", nums[0]);
-  printf("O segundo menor número é: %d\n", nums[1]); 
+    printf("Informe o 2º valor: ");
+    scanf(" %d", &nums[1]);
+
+    printf("Informe o 3º valor: ");
+    scanf(" %d", &nums[2]);
+    
+    printf("Informe o 4º valor: ");
+    scanf(" %d", &nums[3]);
+    
+    printf("\nNúmeros na ordem lida: %d, %d, %d, %d\n", 
+    nums[0], nums[1], nums[2], nums[3]);
+
+    int k = 3;
+    int l = 1;
+    ord[0] = 999999999;
+    ord[3] = -1;
+  
+    for (int i = 0; i < 3; i++) 
+    {
+      for (int j = 0; j < k; j++)
+      {
+        // Verificando o menor número
+        if (nums[i] <= nums[j+l])
+        {        
+          if (nums[i] <= ord[0]) 
+          {          
+            ord[0] = nums[i];          
+          }
+        } 
+        else
+        {
+          if (nums[j+l] <= ord[0]) 
+          {
+            ord[0] = nums[j+l];
+          }
+        }   
+        
+        // Verificando o maior número
+        if (nums[i] >= nums[j+l])
+        {
+          if (nums[i] >= ord[3]) 
+          {
+            ord[3] = nums[i];
+          }
+        } 
+        else
+        {
+          if (nums[j+l] >= ord[3]) 
+          {
+            ord[3] = nums[j+l];
+          }
+        }  
+      }    
+      k--;
+      l++;    
+    }
+  
+    // Identificando os números intermediários 
+    // (só funciona se todos os números forem diferentes)
+    int n = 0;
+    for (int m = 0; m <= 3; m++)
+    {
+      if (nums[m] > ord[0] && nums[m] < ord[3])
+      {
+        rest[n] = nums[m];      
+        n++;
+      }
+    }
+  
+    // Comparando os números restantes e ordenando-os na lista final
+    if (rest[0] < rest[1])
+    {
+      ord[1] = rest[0];
+      ord[2] = rest[1];
+    }
+    else if (rest[0] > rest[1])
+    {
+      ord[1] = rest[1];
+      ord[2] = rest[0];
+    } 
+    else 
+    {
+      ord[1] = rest[0];
+      ord[2] = rest[0];
+    }
+
+    printf("Ordem crescente: %d, %d, %d, %d\n", ord[0], ord[1], ord[2], ord[3]);
+    printf("Ordem decrescente: %d, %d, %d, %d\n\n", ord[3], ord[2], ord[1], ord[0]);
+
+  }
   
   return 0;
 }
